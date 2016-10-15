23a6c22c52d56ee56a2460cb2ff8e23dbcc56df0
Vedmant <vedmant@gmail.com>
2016-10-15 19:30:59 +0300

    Some refacroting for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show]
+
   def index
     @posts = Post.all
   end
@@ -8,12 +10,23 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new params.require(:post).permit(:date, :rationale)
-    @post.save
-    redirect_to @post
+    @post = Post.new post_params
+
+    if @post.save
+      redirect_to @post, notice: 'Your post was created successfully'
+    end
   end
 
   def show
-    @post = Post.find params[:id]
   end
+
+  private
+
+    def post_params
+      params.require(:post).permit(:date, :rationale)
+    end
+
+    def set_post
+      @post = Post.find params[:id]
+    end
 end
