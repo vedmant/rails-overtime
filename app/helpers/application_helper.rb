e7f9c266b08abcc1d6fad743e64ea307d2abfdfa
Vedmant <vedmant@gmail.com>
2016-10-23 23:31:34 +0300

    Added toastr flash notifications

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,15 @@ module ApplicationHelper
     'active' if current_page?(path)
   end
 
+  def toastr_flash
+    flash_messages = []
+    flash.each do |type, message|
+      type = 'success' if type == 'notice'
+      type = 'error'   if type == 'alert'
+      text = "<script>toastr.#{type}('#{message}');</script>"
+      flash_messages << text.html_safe if message
+    end
+    flash_messages.join("\n").html_safe
+  end
+
 end
