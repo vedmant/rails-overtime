c4ca88214ee8eda1881e3229a79c3fb90f682c50
Vedmant <vedmant@gmail.com>
2016-10-29 23:30:12 +0300

    Removed registration functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :posts
-  devise_for :users
+  devise_for :users, skip: [:registration]
   root to: 'static#homepage'
   get 'test', to: 'static#homepage'
 end
