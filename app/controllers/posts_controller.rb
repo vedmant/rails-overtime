ad158e47300ecc2917564c36fca299848e2954b7
Vedmant <vedmant@gmail.com>
2017-06-07 21:10:46 +0300

    Added pagination to posts list

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   def index
-    @posts = Post.posts_by current_user
+    @posts = Post.posts_by(current_user).paginate(:page => params[:page], :per_page => 50)
   end
 
   def new
