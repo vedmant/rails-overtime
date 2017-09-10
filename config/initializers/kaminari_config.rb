e2db2e682283581487e253e32a04f1742bcc2142
Vedmant <vedmant@gmail.com>
2017-09-10 12:42:20 +0300

    Upgraded Rails, fixed issue with Administrate and Kaminari

---
+++ config/initializers/kaminari_config.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+Kaminari.configure do |config|
+  # config.default_per_page = 25
+  # config.max_per_page = nil
+  # config.window = 4
+  # config.outer_window = 0
+  # config.left = 0
+  # config.right = 0
+  # config.page_method_name = :page
+  # config.param_name = :page
+  # config.params_on_first_page = false
+end
+
+# an initializer to handle conflicting kaminari and will_paginate methods
+# config/initializers/will_paginate.rb
+if defined?(WillPaginate)
+  module WillPaginate
+    module ActiveRecord
+      module RelationMethods
+        def per(value = nil) per_page(value) end
+        def total_count() count end
+        def first_page?() self == first end
+        def last_page?() self == last end
+      end
+    end
+    module CollectionMethods
+      alias_method :num_pages, :total_pages
+    end
+  end
+end
