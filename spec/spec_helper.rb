8b5ba1d48a88772f985efa3092afc3808eaa0981
Vedmant <vedmant@gmail.com>
2017-10-05 23:53:03 +0300

    Created SMS stub for tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,4 +18,8 @@ RSpec.configure do |config|
   config.after(:each) { DatabaseCleaner.clean }
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
+
+  config.beforeEach(:each) do
+    stub_const("SmsTool", FakeSms)
+  end
 end
